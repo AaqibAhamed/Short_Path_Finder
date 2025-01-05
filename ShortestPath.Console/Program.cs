be4aab31569da8ff787e234a806e19d1e9edaa4e
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-05 17:38:54 +0530

    Add main logic

--- ShortestPath.Console/Program.cs
+++ ShortestPath.Console/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ShortestPath.Core;
+using ShortestPath.Core.Entities;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        IShortPathRepository shortPathRepository = new ShortPathRepository();
+
+        Console.Write("Enter FROM node: ");
+        var fromNode = Console.ReadLine();
+
+        Console.Write("Enter TO node: ");
+        var toNode = Console.ReadLine();
+
+        var graph = BuildSampleGraph(); // Build the graph with sample data
+
+        var result = shortPathRepository.FindShortestPath(fromNode, toNode, graph);
+
+        Console.WriteLine($"Shortest Path: {string.Join(", ", result.NodeNames)}");
+        
+        Console.WriteLine($"Total Distance: {result.Distance}");
+
+        Console.WriteLine(BuildSampleGraph);
+    }
+
+    static List<Node> BuildSampleGraph()
+    {
+        // Example graph construction
+        var nodeA = new Node { Name = "A" };
+        var nodeB = new Node { Name = "B" };
+        var nodeC = new Node { Name = "C" };
+        var nodeD = new Node { Name = "D" };
+
+        nodeA.Edges.Add(new Edge { TargetNode = nodeB, Weight = 1 });
+        nodeB.Edges.Add(new Edge { TargetNode = nodeC, Weight = 2 });
+        nodeC.Edges.Add(new Edge { TargetNode = nodeD, Weight = 3 });
+
+        return new List<Node> { nodeA, nodeB, nodeC, nodeD };
+    }
+}
