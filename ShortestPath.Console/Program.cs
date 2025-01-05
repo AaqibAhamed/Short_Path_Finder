466133a458ad1d8f1c138da0fa98b1362e1ae88e
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-05 22:38:50 +0530

    Update output pattern

--- ShortestPath.Console/Program.cs
+++ ShortestPath.Console/Program.cs
@@ -9,22 +9,34 @@ class Program
     {
         IShortPathRepository shortPathRepository = new ShortPathRepository();
 
+        System.Console.WriteLine("WelCome To Shortest Path Finder");
+
+        var graph = GraphBuilder.BuildGraphFromUserInput(); // Build the graph with user input
+
         System.Console.Write("Enter FROM node: ");
-        var fromNode = System.Console.ReadLine();
+        var fromNode = System.Console.ReadLine()?.Trim() ?? string.Empty;
 
         System.Console.Write("Enter TO node: ");
-        var toNode = System.Console.ReadLine();
-
-        var graph = BuildSampleGraph(); // Build the graph with sample data
+        var toNode = System.Console.ReadLine()?.Trim() ?? string.Empty;
 
         var result = shortPathRepository.FindShortestPath(fromNode, toNode, graph);
 
-        System.Console.WriteLine($"Shortest Path: {string.Join(", ", result.NodeNames)}");
-        
-        System.Console.WriteLine($"Total Distance: {result.Distance}");
+        if (result == null)
+        {
+            System.Console.WriteLine($"No path exists between {fromNode} and {toNode}.");
+            return;
+        }
+        else
+        {
+            System.Console.WriteLine($"Shortest path from {fromNode} to {toNode}: {string.Join(" -> ", result.NodeNames)}"); // A -> B -> C
 
-        System.Console.WriteLine(BuildSampleGraph);
+            //System.Console.WriteLine($"Shortest path from {fromNode} to {toNode}: {string.Join(" , ", result.NodeNames)}"); A,B,C
+        
+            System.Console.WriteLine($"Total Distance: {result.Distance}");
+        }
+
     }
+
 
     static List<Node> BuildSampleGraph()
     {
