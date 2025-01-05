ef9524e67f6cb02f5c0d8b7bc0e1ad78f7648a1f
AaqibWiki <aaqibgahamed@gmail.com>
2025-01-05 23:25:46 +0530

    Add other 2 methods

--- ShortestPath.Core/Repositories/IShortPathRepository.cs
+++ ShortestPath.Core/Repositories/IShortPathRepository.cs
@@ -6,4 +6,8 @@ namespace ShortestPath.Core.Repositories;
 public interface IShortPathRepository
 {
     ShortestPathDataDTO FindShortestPath(string fromNodeName, string toNodeName, List<Node> graph);
+
+    ShortestPathDataDTO FindShortestPathV2(string fromNodeName, string toNodeName, List<Node> graph);
+
+    ShortestPathDataDTO FindShortestPathV3(string fromNodeName, string toNodeName, List<Node> graph);
 }
